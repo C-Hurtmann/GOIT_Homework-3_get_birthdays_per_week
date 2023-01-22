f7116340e909f3648ec616675b60feec40cde9cb
C-Hurtmann <Constantine2903@gmail.com>
2023-01-22 14:54:18 +0200

    Create unit tests

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
 from collections import defaultdict
+from datetime import datetime, timedelta
 
 
 def birthday_date(users_birthday: datetime):
@@ -30,16 +30,19 @@ def create_report(dict):
     report_date = get_report_date()
     introducion = f"Report was created on {report_date.strftime('%d.%m.%Y')}, because it is the nearest Monday\n"
     print(introducion + '*' * len(introducion))
+    if not len(dict):
+        print('For this week, noone has birthday')
     for k, v in dict.items():
         v = ', '.join(v)
         print(f'{k:<10}: {v}')
 
 if __name__ == '__main__':
     
-    users = [{'name':'Constantine', 'birthday':datetime(year=1995, month=1, day=22)},
+    users1 = [{'name':'Constantine', 'birthday':datetime(year=1995, month=1, day=22)},
          {'name':'Oleg', 'birthday':datetime(year=2000, month=1, day=22)},
          {'name':'Taisa', 'birthday':datetime(year=2005, month=1, day=24)},
          {'name':'Alex', 'birthday':datetime(year=1995, month=1, day=25)},
          {'name':'Dana', 'birthday':datetime(year=1995, month=10, day=21)}]
-    
-    create_report(get_birthdays_per_week(users))
+    users2 = []
+    create_report(get_birthdays_per_week(users1))
+    create_report(get_birthdays_per_week(users2))
